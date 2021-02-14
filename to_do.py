705aed14f74588f0dcafc95d0fb5b23bb64e627b
Fundom team <gameslabaratory@gmail.com>
2021-02-14 14:57:19 +0000

    Добавление события в словарь

--- to_do.py
+++ to_do.py
@@ -6,22 +6,30 @@ show - показать список дел
 exit - выход из приложения
 """
 userAnswer = 0
-to_do = {}
+todo = {}
 
 print("Введите команду")
 print("Список команд - введите help")
 
 while True:
   userAnswer = input().lower()
-
   if userAnswer == "exit":
     print("Программа закрыта")
   elif userAnswer == "add":
+      print("Введите дату в формате ДД.ММ.ГГ.")
+      todoKey = input
+      print("Что нужно сделать?")
+      todoValue = input()
+
+      todo[todoKey] = todoValue
+
       print("Дело добавлено")
   elif userAnswer == "remove":
         print("Дело удалено")
   elif userAnswer == "show":
-    print("Дело показанно")
+    print("\nСписок дел:")
+    for i in sorted( todo.keys() ):
+      print( i + "\t" + todo[todoKey] )
   elif userAnswer == "help":
     print(HELP)
   else:
