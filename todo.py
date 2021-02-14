8be43ac31f47b08f583dce2c2cd5ae3f34defb8d
Vladimir228666 <vovaklinov007@gmail.com>
2021-02-14 14:21:24 +0000

    not work

---
+++ todo.py
@@ -0,0 +1,29 @@
+HELP = """
+help   - список команд
+add    - добавить дело 
+remove - удалить обьект
+show   - показать список дел
+exit   - выход из пртложения
+"""
+userAnswer = 0
+todo = {}
+
+print=("Введите команду")
+print=("список команд - введите help")
+
+while True:
+  userAnswer = input().lower()
+
+  if userAnswer == "exit":
+    print("Программа закрыта") 
+  elif userAnswer == "add":
+    print("Дело добавлено")
+  elif userAnswer == "remove":
+    print("Дело удалено")
+  elif userAnswer == "show":
+    print("Дело показано")
+  elif userAnswer == "help":
+    print(HELP)
+  else:
+    print("Не известная команда")
+    print("список команд - введите help")
