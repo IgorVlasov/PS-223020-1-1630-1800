7e7d69f41c22c5662caa3524969b4d86ac5316e8
Igor Vlasov <vlasov.work@gmail.com>
2021-02-14 14:55:06 +0000

    add события в словарь

--- todo.py
+++ todo.py
@@ -17,11 +17,25 @@ while True:
   if userAnswer == "exit":
     print("Программа закрыта")
   elif userAnswer == "add":
+    print("Введите дату в формате ДД.ММ.ГГГГ")
+    todoKey = input()
+
+    print("Что нужно сделать?")
+    todoValue = input()
+
+    todo[todoKey] = todoValue
+
     print("дело добавлено")
   elif userAnswer == "remove":
     print("дело удалено")
   elif userAnswer == "show":
-    print("дело показано")
+    print("\nСписок дел:")
+
+    for i in sorted( todo.keys() ):
+      print( i + "\t" + todo[todoKey] )
+
+
+    
   elif userAnswer == "help":
     print(HELP)
   else:
